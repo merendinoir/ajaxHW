a705144d56b710a33bf552508130d0818e851d3d
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-04-21 20:16:50 -0400

    set up ajax fxn

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -6,10 +6,26 @@ var topics = [
     "Random"
 ];
 
+function displayGIF() {
+    var topic = $(this).attr("data-name");
+    var APIkey = "&api_key=JwHOoKapA4DZls18ePCOR0T2skwmDImv";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + APIkey + "&limit=10";
+
+// creating AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+        // var imageURL = response.data.image_original_url;
+    })
+}
+displayGIF();
+
+// Looping through topics array and dynamically creating buttons with the class and ID assigned; adding text to the buttons; appending the buttons to our button-view div
 function renderButtons(){
-
     $("#buttons-view").empty();
-
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.addClass("gif-btn");
@@ -19,13 +35,17 @@ function renderButtons(){
     }
 }
 
-$("#add-GIF").on("click", function(event) {
+// pushing the user's choice to our array from the textbox
+$("#addGIF").on("click", function(event) {
     event.preventDefault();
-    var topic = $("#GIF-input").val().trim();
+    var topic = $("#inputGIF").val().trim();
     topics.push(topic);
-    $("#GIF-input").empty();
+    $("#inputGIF").empty();
     renderButtons();
 });
 
+
+
+// Calling fxn to display initial buttons
 renderButtons();
 
